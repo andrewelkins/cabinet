e962267caf1f18c448a356c8013cbfe5f8ffe448
Andrew Elkins <github@andrewelkins.com>
2013-05-15 16:56:57 -0700

    Initial commit

---
+++ src/Andrew13/Laravel4FileUpload/Laravel4FileUploadServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Andrew13\Laravel4FileUpload;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel4FileUploadServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
