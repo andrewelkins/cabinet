d28d62663cc000d6a13ff47ebc0cc2e0bb843461
Andrew Elkins <github@andrewelkins.com>
2013-06-24 17:15:20 -0700

    Add migration

---
+++ src/migrations/2013_06_22_020738_cabinet_setup_uploads_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CabinetSetupUploadsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Creates the uploads table
+        Schema::create('uploads', function($table)
+        {
+            $table->increments('id');
+            $table->string('filename');
+            $table->string('path');
+            $table->string('extension');
+            $table->string('mimetype');
+            $table->integer('user_id')->unsigned()->index();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('uploads');
+    }
+
+}
